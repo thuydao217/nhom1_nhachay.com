2c4f79f5a204c8d2063326b41f29bde8d4129b54
thuydao217 <99631520+thuydao217@users.noreply.github.com>
2022-05-28 16:27:49 +0700

    Add files via upload

--- models/song.model.js
+++ models/song.model.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose')
 
+const SongSchema = mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    singer: { type: String, require: true },
+    image: { type: String, require: true },
+    video: { type: String, require: true },
+  },
+  { timestamps: true }
+)
+
+const Song = mongoose.model('song', SongSchema)
+
+module.exports = Song
