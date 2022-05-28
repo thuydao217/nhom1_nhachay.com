7a53ed3b85f606296b5d317f4a4457e8a123fed1
thuydao217 <99631520+thuydao217@users.noreply.github.com>
2022-05-28 16:32:00 +0700

    Add files via upload

--- services/song.service.js
+++ services/song.service.js
@@ -1,1 +1,28 @@
+// import Model
+const Song = require('../models/song.model')
 
+// thêm mới
+const create = songBody => {
+  // Tạo bài hát
+  const song = Song.create(songBody)
+
+  // Success
+  return song
+}
+
+// Lấy ra tất cả bài hát
+const getAll = () => {
+  const songs = Song.find().sort('-createdAt')
+
+  // Success
+  return songs
+}
+const deleteById = songId => {
+  const song = Song.findByIdAndDelete(songId)
+  return song
+}
+module.exports = {
+  create,
+  getAll,
+  deleteById,
+}
